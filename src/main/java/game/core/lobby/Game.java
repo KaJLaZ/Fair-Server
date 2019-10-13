c7125cb604fde109f62fbcef6846e3e936d72e6e
Antonii Dan <47505016+KaJLaZ@users.noreply.github.com>
2019-10-13 15:18:56 +0300

    fix Lobby Controller

--- src/main/java/game/core/lobby/Game.java
+++ src/main/java/game/core/lobby/Game.java
@@ -10,7 +10,17 @@ public class Game {
     }
 
     public enum Games{
-        APPLE_THEFT, CHOOSE_FATE, DRAW_RUNS, DRINKERS, SEQUENCE
+        APPLE_THEFT(1), CHOOSE_FATE(2), DRAW_RUNS(3), DRINKERS(4), SEQUENCE(5);
+
+        private int numberOfGame;
+
+        public int getNumberOfGame() {
+            return numberOfGame;
+        }
+
+        Games(int numberOfGame) {
+            this.numberOfGame = numberOfGame;
+        }
     }
 
     Games game;
