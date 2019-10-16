06187742fda68f4ff69c5c4ccd924f184c9062cb
Антон <an88ton@gmail.com>
2019-10-16 16:00:41 +0300

    Server for Hit Apple

--- src/main/java/game/controllers/ChooseFateController.java
+++ src/main/java/game/controllers/ChooseFateController.java
@@ -43,8 +43,6 @@ public class ChooseFateController extends Controller{
 
     @RequestMapping(method = RequestMethod.POST, value = "/checkHit")
     public boolean checkHit(double x, double y) {
-        double x1 = x;
-        double y1 = y;
 
         boolean isBelongByX = x >= (GameRoot.CENTER_OF_SQUARE_X - GameRoot.LENGTH_OF_HALF_SIDE)
                 &&( x <= GameRoot.CENTER_OF_SQUARE_X + GameRoot.LENGTH_OF_HALF_SIDE);
@@ -54,9 +52,7 @@ public class ChooseFateController extends Controller{
         if(isBelongByX && isBelongByY){
             mapBase.replace(String.class, "consequence", history.getPosSequence());
             return true;
-        }
-
-        else{
+        } else{
             mapBase.replace(String.class, "consequence", history.getNegSequence());
             return false;
         }
